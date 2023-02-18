aa68e83637752cdfa8e6b31f4f7146f1b1bf8d27
Joseph Esmaail <josephesmaail93@gmail.com>
2023-02-18 23:43:20 +0000

    Add Paint record

---
+++ src/MiniPaintPal.Core/Entities/Paint.cs
@@ -0,0 +1,8 @@
+﻿namespace MiniPaintPal.Core.Entities;
+
+public record Paint
+{
+    public string Type { get; set; }
+    public string Name { get; set; }
+    public string Brand { get; set; }
+}
